6615e1415ed52f90bc972024c8ce6d9ad4ca2db7
fedorovIVT201 <72394670+fedorovIVT201@users.noreply.github.com>
2020-10-06 01:14:20 +0900

    Update cone.js

--- cone.js
+++ cone.js
@@ -7,11 +7,28 @@ function solve() {
 	let d1 = document.getElementById("d1").value;
 	let d2 = document.getElementById("d2").value;
 	let h = document.getElementById("h").value;
+	// высота конуса
+	let h1 = (d2*h)/(d2-d1);
+	// объём ведра
+	if (d1>0 & d2>0 & h>0) {
+		if(d1!=d2){  //если диаметры не равны, то (объем конуса) - (объем усеченного конуса)
+			let v = (coneVolume(d2, h1) - coneVolume(d1, h1-h));
+		}
+		else{
+			 v = Math.PI * ((d1/2)**2) * h; // если диаметры равны, то ищем объем цилиндра
+		}
+		alert("Объем конуса: " + v + " см^3");
+	}
 
-	alert(coneVolume(d1, h));
+	else {
+		alert("Недопустимые данные");
 }
 
-function coneVolume(d, h){
+}
+function coneVolume(d, h) {
 	// вычисление объема полного конуса
-	return d+1;
+	let v = 1/3*Math.PI*((d/2)**2)*h;
+	return v;
 }
+
+
