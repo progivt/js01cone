b387cc7248e23b1b53cc669e9e28737a88f065c9
fedorovIVT201 <72394670+fedorovIVT201@users.noreply.github.com>
2020-10-06 10:49:44 +0900

    решение2

--- cone.js
+++ cone.js
@@ -3,32 +3,29 @@
 // либо сообщение "Недопустимые данные".
 
 function solve() {
-	// получаем данные в виде строк из полей ввода на странице
 	let d1 = document.getElementById("d1").value;
 	let d2 = document.getElementById("d2").value;
 	let h = document.getElementById("h").value;
 	// высота конуса
 	let h1 = (d2*h)/(d2-d1);
 	// объём ведра
+	let v = 0;
 	if (d1>0 & d2>0 & h>0) {
 		if(d1!=d2){  //если диаметры не равны, то (объем конуса) - (объем усеченного конуса)
-			let v = (coneVolume(d2, h1) - coneVolume(d1, h1-h));
+			 v = (coneVolume(d2, h1) - coneVolume(d1, h1-h)); //задача №2
 		}
 		else{
-			 v = Math.PI * ((d1/2)**2) * h; // если диаметры равны, то ищем объем цилиндра
+			 v = Math.PI * ((d1/2)**2) * h; // если диаметры равны, то ищем объем цилиндра // задача №2
 		}
-		alert("Объем конуса: " + v + " см^3");
+		alert("Объем конуса: " + v/1000.0 + " л ");
 	}
-
-	else {
+    else {
 		alert("Недопустимые данные");
-}
-
-}
+    }
+}   
 function coneVolume(d, h) {
 	// вычисление объема полного конуса
+	// задача №1
 	let v = 1/3*Math.PI*((d/2)**2)*h;
 	return v;
-}
-
-
+}        
