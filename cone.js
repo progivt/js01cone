20d6ccc34e0ed496ed1ae0465de426f465e6574e
SavinovKolya <78472552+SavinovKolya@users.noreply.github.com>
2021-05-01 20:12:46 +0900

    Update cone.js

--- cone.js
+++ cone.js
@@ -4,14 +4,19 @@
 
 function solve() {
 	// получаем данные в виде строк из полей ввода на странице
-	let d1 = document.getElementById("d1").value;
-	let d2 = document.getElementById("d2").value;
-	let h = document.getElementById("h").value;
-
-	alert(coneVolume(d1, h));
+	let d1 = (+document.getElementById("d2").value);
+	let d2 = (+document.getElementById("d1").value);
+	let h = (+document.getElementById("h").value);
+	let h1 = d1*h/(d2-d1);
+	let h2 = h1+h;
+	
+	
+	
+	 alert((coneVolume(d2, h2) - coneVolume(d1,h1)) + " Литров");
 }
 
 function coneVolume(d, h){
 	// вычисление объема полного конуса
-	return d+1;
+	return 1/12*Math.PI*d*d*h/1000; 
 }
+
